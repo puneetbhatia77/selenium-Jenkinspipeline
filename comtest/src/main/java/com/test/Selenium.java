8be5b4db12b8607f22f8aa0309c34cd8f07b8a8e
puneetbhatia77 <puneetbhatia77@gmail.com>
2021-08-24 17:19:58 +0530

    Update Selenium.java

--- comtest/src/main/java/com/test/Selenium.java
+++ comtest/src/main/java/com/test/Selenium.java
@@ -1,6 +1,7 @@
 package com.test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Selenium {
 
