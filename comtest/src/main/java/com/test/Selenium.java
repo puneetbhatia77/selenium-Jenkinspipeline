57db5dd6ce1f9aeba3b706833cae75dd83ce8ea7
puneetbhatia77 <puneetbhatia77@gmail.com>
2021-08-24 17:18:05 +0530

    Update Selenium.java

--- comtest/src/main/java/com/test/Selenium.java
+++ comtest/src/main/java/com/test/Selenium.java
@@ -6,7 +6,13 @@ public class Selenium {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","comtest/lib/chromedriver.exe"); // <-- Change this path
-        WebDriver driver = new ChromeDriver();
+	    
+	//to launch in headless mode
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--disable-gpu", "--window-size=1920,1200","--ignore-certificate-errors");
+        WebDriver driver = new ChromeDriver(options);
+		
+	//WebDriver driver = new ChromeDriver();
         String baseUrl = "https://google.com";
         String expectedTitle = "Google";
         String actualTitle = "";
