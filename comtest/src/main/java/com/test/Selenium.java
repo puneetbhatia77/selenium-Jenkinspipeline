f2a1f081396a01d15237546c61ebf56c15f5fded
Puneet <puneetbhatia77@gmail.com>
2021-08-24 09:27:57 +0530

    initial check-in selenium code

---
+++ comtest/src/main/java/com/test/Selenium.java
@@ -0,0 +1,23 @@
+package com.test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Selenium {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","D:\\tools\\chromedriver_win32\\chromedriver.exe"); // <-- Change this path
+        WebDriver driver = new ChromeDriver();
+        String baseUrl = "https://google.com";
+        String expectedTitle = "Google";
+        String actualTitle = "";
+        driver.get(baseUrl);
+        actualTitle = driver.getTitle();
+		System.out.println(actualTitle);
+        if (actualTitle.contentEquals(expectedTitle)){
+            System.out.println("TEST PASSED!");
+        } else {
+            System.out.println("TEST FAILED");
+        }
+        driver.close();
+    }
+}
